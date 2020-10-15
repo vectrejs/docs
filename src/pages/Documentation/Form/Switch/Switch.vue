33b0e493fb6ba01d38df6d4334f31e48e0fecf82
Vadim Tiukov <dev.vatson@gmail.com>
2020-10-15 17:51:06 +0300

    doc: updates documentation for Switch with new info about separate component

--- src/pages/Documentation/Form/Switch/Switch.vue
+++ src/pages/Documentation/Form/Switch/Switch.vue
@@ -1,15 +1,18 @@
 <template>
   <component-view>
     <h2 class="title">Switch</h2>
-    <p>
-      Essentially, you use the same <code>&lt;form-checkbox&gt;</code> component but with <kbd>switch</kbd> type. The
-      rest of the behavior and properties are the same. You can find information about props and slots at the following
-      <router-link to="/pages/form/checkbox">link</router-link>.
-    </p>
+    <p>You have two options for creating a switch:</p>
+    <ol>
+      <li>using the <code>&lt;form-checkbox /&gt;</code> component but with <kbd>switch</kbd> type</li>
+      <li>
+        using separate <code>&lt;form-switch /&gt;</code> which is essentially a wrapper around the
+        <code>&lt;form-checkbox /&gt;</code> with a hardcoded <i>type</i> and the same rest of the behavior and
+        properties. You can find information about props and slots at the following
+        <router-link to="/pages/form/checkbox">link</router-link>.
+      </li>
+    </ol>
 
-    <form-checkbox v-model="light" type="switch">
-      Light
-    </form-checkbox>
+    <form-switch v-model="light"> Light </form-switch>
     Ligth: {{ light ? 'ON' : 'OFF' }}
 
     <prism language="html" :code="basicHtml" />
@@ -18,14 +21,11 @@
     <h3 class="subtitle">Group</h3>
     <p>
       Switch components can be grouped like the checkboxes with <code>&lt;form-checkbox-group&gt;</code> and
-      <kbd>options</kbd> property.
+      <kbd>switch</kbd> type OR <code>&lt;form-switch-group /&gt;</code>.
     </p>
 
-    <form-checkbox-group
-      v-model="conditions"
-      type="switch"
-      :options="['Temperature', 'Humidity', 'Ventilation', 'Lighting']"
-    />
+    <form-switch-group v-model="conditions" :options="['Temperature', 'Humidity', 'Ventilation', 'Lighting']" />
+
     Environment conditions: {{ conditions }}
     <prism language="html" :code="groupHtml" />
     <prism language="javascript" :code="groupJs" />
@@ -45,19 +45,31 @@ export default Vue.extend({
     light: false,
     conditions: [],
 
-    basicHtml: `<form-checkbox type="switch" v-model="light">
+    basicHtml: `<form-switch v-model="light"> Light </form-switch>
+
+<!-- OR same with <form-checkbox type="switch" ... /> -->
+
+<form-checkbox type="switch" v-model="light">
   Light
 </form-checkbox>
+
 Ligth: {{ light ? 'ON' : 'OFF' }}`,
     basicJs: `export default {
   data: () => ({
     light: false,
   }),
 }`,
-    groupHtml: `<form-checkbox-group
+    groupHtml: `<form-switch-group 
+  v-model="conditions" 
+  :options="['Temperature', 'Humidity', 'Ventilation', 'Lighting']" 
+/>
+
+<!-- OR same with <form-checkbox-group type="switch" ... /> -->
+
+<form-checkbox-group
   type="switch"
-  :options="['Temperature', 'Humidity', 'Ventilation', 'Lighting']"
   v-model="conditions"
+  :options="['Temperature', 'Humidity', 'Ventilation', 'Lighting']"
 />
 Environment conditions: {{ conditions }}`,
     groupJs: `export default {
@@ -65,7 +77,16 @@ Environment conditions: {{ conditions }}`,
     conditions: [], // Should be an array
   }),
 }`,
-    nestedHtml: `<form-checkbox-group type="switch" v-model="conditions">
+    nestedHtml: `<form-switch-group v-model="conditions">
+  <form-switch value="Temperature" />
+  <form-switch value="Humidity" />
+  <form-switch value="Ventilation" />
+  <form-switch value="Lighting" />
+</form-switch-group>
+
+<!-- OR same with <form-checkbox-group type="switch" ... /> -->
+    
+<form-checkbox-group type="switch" v-model="conditions">
   <form-checkbox value="Temperature"/>
   <form-checkbox value="Humidity"/>
   <form-checkbox value="Ventilation"/>
